c518897c8a210713e7b4902982e16bb6b49354e3
akhil-ankola <akhilankola07@gmail.com>
2024-08-01 23:19:33 +0530

    commit

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>React First Project</h1>
+      <h1>React First Project</h1> 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -25,9 +25,6 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
