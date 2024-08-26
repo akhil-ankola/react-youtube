182829f31f5903774067fd2aa9fa926dada515b2
akhil-ankola <akhilankola07@gmail.com>
2024-08-27 01:43:12 +0530

    react 5

---
+++ 01reactvite-counter/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+  const[count, setcount] = useState(0);
+
+  function addCountHandler(){
+    return setcount(count+1);
+  }
+  function removeCountHandler(){
+    return setcount(count-1);
+  }
+  function counterResetHandler(){
+    return setcount(0);
+  }
+
+  return (
+    <>
+      <h1>Chai with React</h1>
+      <h2>Counter : {count}</h2>
+      <div>
+        <button onClick={addCountHandler}>Add</button>
+        <button onClick={counterResetHandler}>Reset</button>
+        <button onClick={removeCountHandler}>Remove</button>
+      </div>
+    </>
+  )
+}
+
+export default App
